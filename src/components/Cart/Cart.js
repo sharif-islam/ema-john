1a6226bc4939fbb5ace1214e9723dda6fdbe379a
sharif-islam <abmsharif555@gmaile.com>
2021-07-13 04:05:12 +0600

    new update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 import "./Cart.css";
 
 const Cart = (props) => {
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,7 +13,7 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
-  const [products, setProducts] = useState(first10);
+  const products = first10;
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
